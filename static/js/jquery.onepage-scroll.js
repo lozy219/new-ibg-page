7db4758f711e238bd29e88fc46ea4b38a67d796e
Lei Mingyu <uygnim.iel@gmail.com>
2015-02-21 17:59:36 +0800

    nav bar fix

--- static/js/jquery.onepage-scroll.js
+++ static/js/jquery.onepage-scroll.js
@@ -125,72 +125,85 @@
     $.fn.moveDown = function() {
       var el = $(this)
       index = $(settings.sectionContainer +".active").data("index");
-      current = $(settings.sectionContainer + "[data-index='" + index + "']");
-      next = $(settings.sectionContainer + "[data-index='" + (index + 1) + "']");
-      if(next.length < 1) {
-        if (settings.loop == true) {
-          pos = 0;
-          next = $(settings.sectionContainer + "[data-index='1']");
-        } else {
-          return
+      if ((index >= 0) && (index <= 1)) {
+        current = $(settings.sectionContainer + "[data-index='" + index + "']");
+        next = $(settings.sectionContainer + "[data-index='" + (index + 1) + "']");
+
+        $(".navbar-hr").removeAttr("id");
+        $(".navbar-hr").attr("id", "hr-" + (index + 1));
+
+        if(next.length < 1) {
+          if (settings.loop == true) {
+            pos = 0;
+            next = $(settings.sectionContainer + "[data-index='1']");
+          } else {
+            return
+          }
+
+        }else {
+          pos = (index * 100) * -1;
+        }
+        if (typeof settings.beforeMove == 'function') settings.beforeMove( next.data("index"));
+        current.removeClass("active")
+        next.addClass("active");
+        if(settings.pagination == true) {
+          $(".onepage-pagination li a" + "[data-index='" + index + "']").removeClass("active");
+          $(".onepage-pagination li a" + "[data-index='" + next.data("index") + "']").addClass("active");
         }
 
-      }else {
-        pos = (index * 100) * -1;
-      }
-      if (typeof settings.beforeMove == 'function') settings.beforeMove( next.data("index"));
-      current.removeClass("active")
-      next.addClass("active");
-      if(settings.pagination == true) {
-        $(".onepage-pagination li a" + "[data-index='" + index + "']").removeClass("active");
-        $(".onepage-pagination li a" + "[data-index='" + next.data("index") + "']").addClass("active");
-      }
+        $("body")[0].className = $("body")[0].className.replace(/\bviewing-page-\d.*?\b/g, '');
+        $("body").addClass("viewing-page-"+next.data("index"))
 
-      $("body")[0].className = $("body")[0].className.replace(/\bviewing-page-\d.*?\b/g, '');
-      $("body").addClass("viewing-page-"+next.data("index"))
-
-      if (history.replaceState && settings.updateURL == true) {
-        var href = window.location.href.substr(0,window.location.href.indexOf('#')) + "#" + (index + 1);
-        history.pushState( {}, document.title, href );
+        if (history.replaceState && settings.updateURL == true) {
+          var href = window.location.href.substr(0,window.location.href.indexOf('#')) + "#" + (index + 1);
+          history.pushState( {}, document.title, href );
+        }
+        el.transformPage(settings, pos, next.data("index"));
       }
-      el.transformPage(settings, pos, next.data("index"));
     }
 
     $.fn.moveUp = function() {
       var el = $(this)
       index = $(settings.sectionContainer +".active").data("index");
-      current = $(settings.sectionContainer + "[data-index='" + index + "']");
-      next = $(settings.sectionContainer + "[data-index='" + (index - 1) + "']");
+      if ((index >= 0) && (index <= 2)) {
+        current = $(settings.sectionContainer + "[data-index='" + index + "']");
+        next = $(settings.sectionContainer + "[data-index='" + (index - 1) + "']");
 
-      if(next.length < 1) {
-        if (settings.loop == true) {
-          pos = ((total - 1) * 100) * -1;
-          next = $(settings.sectionContainer + "[data-index='"+total+"']");
-        }
-        else {
-          return
-        }
-      }else {
-        pos = ((next.data("index") - 1) * 100) * -1;
-      }
-      if (typeof settings.beforeMove == 'function') settings.beforeMove(next.data("index"));
-      current.removeClass("active")
-      next.addClass("active")
-      if(settings.pagination == true) {
-        $(".onepage-pagination li a" + "[data-index='" + index + "']").removeClass("active");
-        $(".onepage-pagination li a" + "[data-index='" + next.data("index") + "']").addClass("active");
-      }
-      $("body")[0].className = $("body")[0].className.replace(/\bviewing-page-\d.*?\b/g, '');
-      $("body").addClass("viewing-page-"+next.data("index"))
+        $(".navbar-hr").removeAttr("id");
+        $(".navbar-hr").attr("id", "hr-" + (index - 1));
 
-      if (history.replaceState && settings.updateURL == true) {
-        var href = window.location.href.substr(0,window.location.href.indexOf('#')) + "#" + (index - 1);
-        history.pushState( {}, document.title, href );
+        if(next.length < 1) {
+          if (settings.loop == true) {
+            pos = ((total - 1) * 100) * -1;
+            next = $(settings.sectionContainer + "[data-index='"+total+"']");
+          }
+          else {
+            return
+          }
+        }else {
+          pos = ((next.data("index") - 1) * 100) * -1;
+        }
+        if (typeof settings.beforeMove == 'function') settings.beforeMove(next.data("index"));
+        current.removeClass("active")
+        next.addClass("active")
+        if(settings.pagination == true) {
+          $(".onepage-pagination li a" + "[data-index='" + index + "']").removeClass("active");
+          $(".onepage-pagination li a" + "[data-index='" + next.data("index") + "']").addClass("active");
+        }
+        $("body")[0].className = $("body")[0].className.replace(/\bviewing-page-\d.*?\b/g, '');
+        $("body").addClass("viewing-page-"+next.data("index"))
+
+        if (history.replaceState && settings.updateURL == true) {
+          var href = window.location.href.substr(0,window.location.href.indexOf('#')) + "#" + (index - 1);
+          history.pushState( {}, document.title, href );
+        }
+        el.transformPage(settings, pos, next.data("index"));
       }
-      el.transformPage(settings, pos, next.data("index"));
     }
 
     $.fn.moveTo = function(page_index) {
+      $(".navbar-hr").removeAttr("id");
+      $(".navbar-hr").attr("id", "hr-" + page_index);
       current = $(settings.sectionContainer + ".active")
       next = $(settings.sectionContainer + "[data-index='" + (page_index) + "']");
       if(next.length > 0) {
