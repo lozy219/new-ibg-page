9ec411bfb222a9f38558de87730a442221d5c52f
Lei Mingyu <uygnim.iel@gmail.com>
2015-04-04 14:25:11 +0800

    background

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,15 +1,20 @@
 $(document).ready(function(){
-	$(".main").onepage_scroll({
-		sectionContainer: "section",
-		responsiveFallback: 600,
-		loop: false
-	});
+	resizeBackground();
+	$(window).resize(resizeBackground);
 
-	$(".scrollIndicator").on("click", function() {
-		$(".main").moveTo(2);
+	$('#logo').click(function() {
+		$(".main").moveTo(1);
 	});
-
-	 $('#logo').click(function() {
-        $(".main").moveTo(1);
-    });
 });
+
+function resizeBackground() {
+	var width = $(window).width();
+	var height = $(window).height();
+	if (width * 0.68 >= height) {
+		$('#homepage-background').css('width', '100%');
+		$('#homepage-background').css('height', 'auto');
+	} else {
+		$('#homepage-background').css('height', '100vh');
+		$('#homepage-background').css('width', 'auto');
+	}
+}
